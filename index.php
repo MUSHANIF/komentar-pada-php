6d80ba30a32b8293f5dbf0bf7bed6647bd5ae2f2
musthafahanif <hanifmusthafa2005@gmail.com>
2021-12-21 10:12:00 +0700

    menambahkan sweeralert

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ if (empty($_SESSION['csrf_token'])) {
 	<meta charset="utf-8">
 	<!-- Csrf Token -->
 	<meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
 
 	<title></title>
@@ -24,7 +26,8 @@ if (empty($_SESSION['csrf_token'])) {
  
 <div class="container mb-3">
 	<h2 align="center" style="margin: 60px 10px 10px 10px;">TEST KOMENTAR</h2><hr>
-	<form method="POST" id="form_komen">
+	
+	<form method="POST" id="form_komen" name="Contact-Form">
 		<div class="form-group">
 			<input type="text" name="nama_pengirim" id="nama_pengirim" class="form-control" placeholder="Masukkan Nama" />
 		</div>
@@ -33,8 +36,10 @@ if (empty($_SESSION['csrf_token'])) {
 		</div>
 		<div class="form-group">
 			<input type="hidden" name="komentar_id" id="komentar_id" value="0" />
-			<input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+			<!-- <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" /> -->
 		</div>
+		<button type="submit" class="btn btn-primary btn-kirim" id="submit">Submit</button>
+    
 	</form>
 	<hr>
 	<h4 class="mb-3">Komentar :</h4>
@@ -50,10 +55,13 @@ if (empty($_SESSION['csrf_token'])) {
 </div>
 </footer>
 
-
-
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
 <script>
+
 		$(document).ready(function(){
 			//Mengirimkan Token Keamanan
 			$.ajaxSetup({
@@ -78,6 +86,11 @@ if (empty($_SESSION['csrf_token'])) {
     					method:"POST",
     					data:form_data,
     					success:function(data){
+    						Swal.fire(
+									  'Sukses!',
+									  'Komentar anda berhasil ditambahkan!',
+									  'success'
+									);
     						$('#form_komen')[0].reset();
     						$('#komentar_id').val('0');
     						load_comment();
